1581cb0f189ea0b0af3c36d5d585bdafc6fcec4c
comcong <sanare7844@naver.com>
2023-10-21 23:07:23 +0900

    2023/10/21 멀티 페이지

--- pages/매물_현황.py
+++ pages/매물_현황.py
@@ -1,3 +1,4 @@
 import streamlit as st
-if st.session_state.maemul_df.any():
+if st.session_state.maemul_df != None:
     st.dataframe(st.session_state.maemul_df, hide_index=True)
+    st.session_state.maemul_df = None
