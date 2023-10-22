dd23d00ef4c13be3cc849cda99e3793856af3cec
comcong <sanare7844@naver.com>
2023-10-22 18:32:18 +0900

    2023/10/22

--- pages/매물_현황.py
+++ pages/매물_현황.py
@@ -5,10 +5,10 @@ if 'maemul_df' in st.session_state:
     st.dataframe(st.session_state.maemul_df, hide_index=True)
 
 
-    # @st.cache
+    @st.cache_data
     def convert_df(df):
         # IMPORTANT: Cache the conversion to prevent computation on every rerun
-        return df.to_csv().encode('utf-8')
+        return df.to_csv().encode('euc-kr')
 
     csv = convert_df(st.session_state.maemul_df)
 
@@ -16,7 +16,6 @@ if 'maemul_df' in st.session_state:
         label="Download data as CSV",
         data=csv,
         file_name='large_df.csv',
-        # mime='text/csv',
+        mime='text/csv',
     )
 
-    st.write('신명진')
