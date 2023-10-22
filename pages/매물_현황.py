fedc391248a87690577f33c39540251f08e918b7
comcong <sanare7844@naver.com>
2023-10-22 12:35:14 +0900

    2023/10/22

--- pages/매물_현황.py
+++ pages/매물_현황.py
@@ -1,3 +1,5 @@
 import streamlit as st
 
-st.dataframe(st.session_state.maemul_df, hide_index=True)
+
+if 'maemul_df' in st.session_state:
+    st.dataframe(st.session_state.maemul_df, hide_index=True)
